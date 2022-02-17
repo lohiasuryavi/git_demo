1921199744998f3debcdc51333b0e59547fb35ee
Suryavi <lohiasuryavi@gmail.com>
2022-02-17 10:02:25 +0530

    added some comment in add function

--- math.py
+++ math.py
@@ -1,7 +1,10 @@
 #I need to implement this
 def add(x,y):
+    #.....
+    #if x and y both are intregeer then add them
     if(type(x)==int and type(y) == int):
         return x+y
+    #otherwise return none
     return None
   
 #Bob needs to implement this
